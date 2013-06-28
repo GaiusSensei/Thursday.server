cff48e59719e6e6577d2d44a809502eeecf2516e
Erik Gaius Capistrano <gaiussensei@gmail.com>
2013-06-28 07:59:38 -0400

    Added CORS, TOR.init

--- app.js
+++ app.js
@@ -9,21 +9,49 @@ var app = module.exports = express();
 // Database
 // mongoose.connect('mongodb://' + process.env.IP +'/thursdayDB');
 
+// CORS
+var allowedDomains = ["*"];
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', allowedDomains.join(','));
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'content-type');
+    next();
+};
 // Config
 app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+    app.use(express.static(path.join(application_root, "public")));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-// Test API
-app.get('/api', function (req, res) {
+// Thursday API
+app.all('/api', function (req, res, next) {
     res.send('Thursday.server is up and running!');
 });
-app.get('/api/test', function (req, res) {
-    tor.test(function(response){ res.send(JSON.stringify(response)); });
+app.all('/api/test', function (req, res, next) {
+    tor.test(function testF(response){ 
+        res.send(JSON.stringify(response)); 
+    });
+});
+app.post('/api/init', function(req, res, next) {
+    if ((!req.body.email) || (!req.body.passwd)) {
+        res.send(400, "Thursday.error: Email and Password required.");
+    } else { 
+        tor.init(
+            req.body.email,
+            req.body.passwd,
+            function initF(response){
+                if (response.error) {
+                    res.send(401);
+                } else {
+                    res.send(JSON.stringify(response.data)); 
+                }
+            }
+        );
+    }
 });
 
 // Launch server
--- torjs/tor.js
+++ torjs/tor.js
@@ -1,15 +1,19 @@
 var util = require("./tor.util.js");
 
-exports.test = function test(callback) {
+exports.test = function testF(callback) {
+    this.init('test@test.test','test123456',callback);
+};
+
+exports.init = function initF(email, password, callback) {
     var credentials = {
         "client":"Thursday",
         "accountType":"HOSTED_OR_GOOGLE",
         "service":"reader",
-        "Email":"test@test.test",
-        "Passwd":"test1234",
+        "Email":email,
+        "Passwd":password,
         "output":"json"
     };
     
     util.post("/reader/api/0/accounts/ClientLogin",
-        credentials, null, callback);
+        credentials, null, callback);    
 };
